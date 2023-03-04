2220be262ccabf961306d32a999e221b359a87ec
Zach Wilcox <Zacdwil@hotmail.com>
2023-03-04 06:43:06 +0000

    commit

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,10 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max = -2147483647;
+        for(int value : arr){
+            if(value > max) max = value;
+        }
+        return max;
     }
 }
